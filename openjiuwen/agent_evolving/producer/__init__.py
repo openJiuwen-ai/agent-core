4c7bcb80029fcdc2e85d20a7d9007662faf4e79a
deyang <lideyang2@huawei.com>
2026-02-07 12:45:24 +0800

    feat(agent_evolving): add UpdateProducer protocol and producer implementations
    Refs: #338
    
    Co-authored-by: deyang<lideyang2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !399 feat: (agent_evolving)add UpdateProducer protocol and producer implementations
    
    From: @deyang 
    Reviewed-by: @deyang, @yangzequ, @alan_cheng 
    
    
    See merge request: openJiuwen/agent-core!399

---
+++ openjiuwen/agent_evolving/producer/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+
+from openjiuwen.agent_evolving.producer.protocol import UpdateProducer
+from openjiuwen.agent_evolving.producer.single_dim import SingleDimProducer
+from openjiuwen.agent_evolving.producer.multi_dim import MultiDimProducer
+
+__all__ = [
+    "UpdateProducer",
+    "SingleDimProducer",
+    "MultiDimProducer",
+]
