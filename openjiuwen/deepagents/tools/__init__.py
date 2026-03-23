1f0e0d74529a47726772187c59015036bb3f4b84
AFSDFASD <gaozhiting1@huawei.com>
2026-03-23 19:35:51 +0800

    feat(deepagent): add progressive tool rail
    Co-authored-by: zhiting gao<gaozhiting1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !796 feat(deepagent): add progressive tool rail
    
    From: @AFSDFASD 
    Reviewed-by: @SnapeK, @iamcandiceguo 
    
    
    See merge request: openJiuwen/agent-core!796

--- openjiuwen/deepagents/tools/__init__.py
+++ openjiuwen/deepagents/tools/__init__.py
@@ -11,6 +11,9 @@ from openjiuwen.deepagents.tools.filesystem import (
     ListDirTool,
     GrepTool,
 )
+from openjiuwen.deepagents.tools.list_skill import ListSkillTool
+from openjiuwen.deepagents.tools.load_tools import LoadToolsTool
+from openjiuwen.deepagents.tools.search_tools import SearchToolsTool
 from openjiuwen.deepagents.tools.shell import BashTool
 from openjiuwen.deepagents.tools.vision import (
     ImageOCRTool,
@@ -40,6 +43,9 @@ __all__ = [
     "ListDirTool",
     "GrepTool",
     "BashTool",
+    "ListSkillTool",
+    "SearchToolsTool",
+    "LoadToolsTool",
     "ImageOCRTool",
     "VisualQuestionAnsweringTool",
     "create_vision_tools",
