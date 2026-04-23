7bd85bfea4a9c6618d7b75cd68593097f45b5fda
Ziliang_Zhou <zhouziliang4@huawei.com>
2026-04-23 19:48:06 +0800

    feat: memory add openjiuwen memory external & openviking memory external
    Refs: #705
    
    Co-authored-by: Ziliang_Zhou<zhouziliang4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1133 feat(memory): memory add openjiuwen memory external & openviking memory external
    
    From: @Ziliang_Zhou 
    Reviewed-by: @alan_cheng, @wangxiaolong100, @SnapeK 
    
    
    See merge request: openJiuwen/agent-core!1133

--- openjiuwen/core/memory/external/__init__.py
+++ openjiuwen/core/memory/external/__init__.py
@@ -2,7 +2,9 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 """External memory provider subsystem."""
 
-from openjiuwen.core.memory.external.mem0_provider import Mem0MemoryProvider
 from openjiuwen.core.memory.external.provider import MemoryProvider
+from openjiuwen.core.memory.external.mem0_provider import Mem0MemoryProvider
+from openjiuwen.core.memory.external.openjiuwen_memory_provider import OpenJiuwenMemoryProvider
+from openjiuwen.core.memory.external.openviking_memory_provider import OpenVikingMemoryProvider
 
-__all__ = ["Mem0MemoryProvider", "MemoryProvider"]
+__all__ = ["MemoryProvider", "OpenJiuwenMemoryProvider", "OpenVikingMemoryProvider", "Mem0MemoryProvider"]
